0dff8f804f1213b131f1bbf8bf236faa6a26bbb0
tthrasher <tthrasher@gmail.com>
2021-04-13 21:16:22 -0400

    Completed tutorial instructions. Left in the most interesting and educational code.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(text)
+            .font(.largeTitle)
             .padding()
+            // .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
+// Example given with generics, which is a feature I definitely don't understand yet
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Not first")
+                .foregroundColor(.yellow)
+            Text("Undertitle")
+                .titleStyle()
+                .watermarked(with: "Hacking with Swift")
+            GridStack(rows: 4, columns: 4) { row, col in
+                Text("R\(row) C\(col)")
+            }
+        }
     }
 }
 
